281c84f5bb24a5b0191b7ed3df94ba48e9870b35
Gokul Raj V <gokulrajv108@gmail.com>
2024-11-26 23:28:37 +0530

    Restructuring project for PYPI release

--- dvault/constants.py
+++ dvault/constants.py
@@ -2,7 +2,7 @@ import os
 
 
 class Constants:
-    APP_VERSION = "0.1"
+    APP_VERSION = "1.0"
     MY_BIRTH_DAY_UNIX_SECONDS = 860201400
     MY_BIRTH_DAY_BYTE_ARRAY = b"05 April 1997 AD"
     SALT = b"\x18\xec7$\xd9\x85\xc87$i\xa3\xfeZ-\xd49\xe2\xb1$\x11\xc3C\xe5\xf1\xc0\xf8\x136%\xd7\xcd!"
